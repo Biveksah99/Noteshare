19f1389abd5e2282a594b6ec714c54c7fa752f76
Bivek sah <sbivek215@gmail.com>
2025-04-29 07:27:04 +0000

    the text which is uploaded in description shows attractive font

--- src/app/view-note/page.tsx
+++ src/app/view-note/page.tsx
@@ -146,7 +146,8 @@ const ViewNotePage = () => {
         </CardHeader>
         <CardContent>
           {/* Use whitespace-pre-wrap to respect newlines and spacing in description */}
-          <CardDescription className="mb-4 whitespace-pre-wrap">
+          {/* Apply serif font for a different style */}
+          <CardDescription className="mb-4 whitespace-pre-wrap font-serif text-base">
             {displayDescription}
             {isLongDescription && !isDescriptionExpanded && (
               <Button
